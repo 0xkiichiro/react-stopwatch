2a58eaa8b1b19bb8137c935fb95e26633895ef61
0xkiichiro <barisaytimur@gmail.com>
2022-06-29 19:21:52 +0300

    progress

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import "./Counter.css";
 
 function Counter() {
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
   return (
     <div className="stopwatch-container">
       <div id="timerDisplay">
@@ -10,8 +27,15 @@ function Counter() {
         </p>
       </div>
       <div className="buttons">
-        <button id="start">start</button>
-        <button id="reset">reset</button>
+        <button id="start" onClick={() => setTimerOn(true)}>
+          start
+        </button>
+        <button id="start" onClick={() => setTimerOn(false)}>
+          pause
+        </button>
+        <button id="reset" onClick={() => setTime(0)}>
+          reset
+        </button>
       </div>
     </div>
   );
